db68609ab687d4b203b33362b06ada1bf4ee7239
Max <501563@student.fontys.nl>
2022-06-08 17:04:02 +0200

    Added MessageController class and updated announcement and complaint forms to use it.

---
+++ StudentHousingManagement/StudentHousingManagement/Classes/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentHousingManagement
+{
+    public class MessageController
+    {
+        public void NewAnnouncementBuilding(User user, string header, string body, DateTime dateTime)
+        {
+                user.House.Building.Announcements.Add(new Message(header, body, user, dateTime));
+        }
+        public void NewAnnouncementHouse(User user, string header, string body, DateTime dateTime)
+        {
+            user.House.Announcements.Add(new Message(header, body, user, dateTime));
+        }
+        public void NewComplaint(User user, string header, string body, DateTime dateTime)
+        {
+            user.House.Building.Complaints.Add(new Message(header, body, user, dateTime));
+        }
+    }
+}
